4af09962f9deefd93f2112c454fb7842db9886fa
Мирослав Гришин <sergiy.1911@gmail.com>
2016-03-09 14:55:30 +0200

    Fixed few bugs in pull request

--- SharingThings/settings.py
+++ SharingThings/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'SharingWeb',
-    'fixture_magic',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -80,11 +79,11 @@ WSGI_APPLICATION = 'SharingThings.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DBNAME', default='sharing_dev'),
-        'USER': os.environ.get('DBUSER', default='sharing_user'),
-        'PASSWORD': os.environ.get('DBPASSWORD', default='sharing_password'),
-        'HOST': os.environ.get('DBHOST', default='localhost'),
-        'PORT': os.environ.get('DBPORT', default='')
+        'NAME': 'sharing_dev',
+        'USER': 'sharing_user',
+        'PASSWORD': 'sharing_password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
